e6d26e09f3d89db6ca95adb1892f2dc826f68391
mairaramirez <mairaramirez9898@gmail.com>
2025-06-17 10:31:59 -0300

    Ejercicio 2 finalizado

--- js/ejercicio-b.js
+++ js/ejercicio-b.js
@@ -1,8 +1,11 @@
-console.log("funciona el ejercicio B")
+/* console.log("Ejercicio B")
 
-var emoji=" 🐈"
-var emoji2=" 🐾" 
+var emoji=" 🐈";
+var emoji2=" 🐾"; 
+var cantidadDeGatos = parseInt(prompt("Ingrese un cantidad de gatos")); 
+var cantidadDePasos = parseInt(prompt("Ingrese un cantidad de pasos"));
+var patitas= emoji2.repeat(cantidadDePasos);
 
- var cantidadDeGatos = parseInt(prompt("Ingrese un cantidad de gatos"));
- var cantidadDePasos = parseInt(prompt("Ingrese un cantidad de pasos"));
-
+for (var i = 1; i <= cantidadDeGatos; i++) {    
+    console.log("Gato #" + i + emoji + patitas)    
+} */
--- js/ejercicio-c.js
+++ js/ejercicio-c.js
@@ -1,1 +1,9 @@
 console.log("funciona el ejercicio C")
+
+var cantidadDeGatos = parseInt(prompt("Ingrese un cantidad de gatos"));
+var cantidadDePasos = parseInt(prompt("Ingrese un cantidad de pasos"));
+
+var emoji=" 🐈";
+var emoji2=" 🐾"; 
+var emoji3=" ⬛";
+
